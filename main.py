6810fc36f8d553bcecfe7ef54f975ee652f9bbb0
221RDB452 <124876380+221RDB452@users.noreply.github.com>
2023-03-19 12:37:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,42 @@
 # python3
+import heapq
 
 def parallel_processing(n, m, data):
+    # Initialize an empty heap with n threads
+    threads = [(0, i) for i in range(n)]
+    heapq.heapify(threads)
+    
+    # Initialize the output list
     output = []
-    # TODO: write the function for simulating parallel tasks, 
-    # create the output pairs
-
+    
+    # Process each job in the input data
+    for i in range(m):
+        # Get the next job time from the input data
+        job_time = data[i]
+        
+        # Get the thread with the earliest finish time
+        thread_time, thread_idx = heapq.heappop(threads)
+        
+        # Add the output pair for this job and thread
+        output.append((thread_idx, thread_time))
+        
+        # Update the thread's finish time
+        finish_time = thread_time + job_time
+        heapq.heappush(threads, (finish_time, thread_idx))
     return output
 
 def main():
-    # TODO: create input from keyboard
-    # input consists of two lines
-    # first line - n and m
-    # n - thread count 
-    # m - job count
-    n = 0
-    m = 0
-
-    # second line - data 
-    # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
-    data = []
-
-    # TODO: create the function
-    result = parallel_processing(n,m,data)
+    # Get input from the user
+    n, m = map(int, input().split())
+    data = list(map(int, input().split()))
     
-    # TODO: print out the results, each pair in it's own line
-
-
+    # Run the parallel processing algorithm
+    result = parallel_processing(n, m, data)
+    
+    # Print the output pairs
+    for thread_idx, start_time in result:
+        print(thread_idx, start_time)
 
 if __name__ == "__main__":
     main()
+
